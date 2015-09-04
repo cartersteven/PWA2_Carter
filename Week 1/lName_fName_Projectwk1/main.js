c65af4dc5ef50f1be6b9943a4837a9f34f4e0e2c
Steven Carter <stevencarter@fullsail.edu>
2015-09-04 17:03:47 -0400

    Added code to find and display the position of the UL element

--- Week 1/lName_fName_Projectwk1/main.js
+++ Week 1/lName_fName_Projectwk1/main.js
@@ -5,7 +5,12 @@ Date: 4 September 2015
 */
 
 $(document).ready(function(){    /*Function only activates if the DOM is complete*/
+  console.log("START OF TARGET 1");/*Start Target 1*/
+
   $('#contentleft > ul').css('backgroundColor', 'orange');    /*changes the background color of the links list to orange*/
 
+  var positionViaOffset = $('#contentleft > ul').offset();
+  console.log("The top position via offset is left: " + positionViaOffset.left + ", top: " + positionViaOffset.top);
 
+  
 });
