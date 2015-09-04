83da564db211d4e9dba82c65ccc58da018fd5b2a
Steven Carter <stevencarter@fullsail.edu>
2015-09-04 16:42:55 -0400

    Target1: First attempt changing background color

--- Week 1/lName_fName_Projectwk1/main.js
+++ Week 1/lName_fName_Projectwk1/main.js
@@ -3,3 +3,9 @@ Description: Project 1: Manipulating the DOM
 Author: Steven Carter | 0004502683
 Date: 4 September 2015
 */
+
+$(document).ready(function(){
+  $('#contentleft > ul').css(background-color(orange));
+
+
+});
