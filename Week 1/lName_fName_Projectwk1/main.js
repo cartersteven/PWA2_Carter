1754822c8dc3c11f0f2fdb9aa3695a971b8c19ea
Steven Carter <stevencarter@fullsail.edu>
2015-09-04 16:31:43 -0400

    Titled the mainJS page

--- Week 1/lName_fName_Projectwk1/main.js
+++ Week 1/lName_fName_Projectwk1/main.js
@@ -1,6 +1,5 @@
 /*
-Description: DESCRIPTION INFO GOES HERE
-Author: Name
-Date: DATE GOES HERE
+Description: Project 1: Manipulating the DOM
+Author: Steven Carter | 0004502683
+Date: 4 September 2015
 */
-
