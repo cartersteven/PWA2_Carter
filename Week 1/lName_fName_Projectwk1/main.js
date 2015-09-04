944d69276786d9d3a6d7eb46541c5b6d9afabb13
Steven Carter <stevencarter@fullsail.edu>
2015-09-04 17:26:52 -0400

    TARGET2 | added orders from target 2. Changed all single quotes to double.

--- Week 1/lName_fName_Projectwk1/main.js
+++ Week 1/lName_fName_Projectwk1/main.js
@@ -7,21 +7,23 @@ Date: 4 September 2015
 $(document).ready(function(){    /*Function only activates if the DOM is complete*/
   console.log("~~~~~START OF TARGET 1~~~~~");/*Start Target 1*/
 
-    $('#contentleft > ul').css('backgroundColor', 'orange');    /*Changes the background color of the links list to orange*/
+    $("#contentleft > ul").css("backgroundColor", "orange");    /*Changes the background color of the links list to orange*/
 
-    var positionViaOffset = $('#contentleft > ul').offset();/*Sets the position of the left side UL (using offset()) to the variable positionViaOffset*/
+    var positionViaOffset = $("#contentleft > ul").offset();/*Sets the position of the left side UL (using offset()) to the variable positionViaOffset*/
     console.log("The top position via offset is left: " + positionViaOffset.left + ", top: " + positionViaOffset.top); /*Logs the positon to the console.*/
 
-    var positionViaPosition = $('#contentleft > ul').position();/*Sets the position of the left side UL (using position()) to the variable positionViaPosition*/
+    var positionViaPosition = $("#contentleft > ul").position();/*Sets the position of the left side UL (using position()) to the variable positionViaPosition*/
     console.log("The top position via position is left: " + positionViaPosition.left + ", top: " + positionViaPosition.top);/*Logs the positon to the console.*/
 
   console.log("~~~~~END OF TARGET 1~~~~~");/*End Target 1*/
-
+  console.log(" ")
 
 
 
 
   console.log("~~~~~START OF TARGET 2~~~~~");/*Start Target 2*/
-
-    
+  console.log("No console events in Target 2.");
+    $("#contentleft > ul > li").eq(1).css("marginLeft", "30px");
+  console.log("~~~~~END OF TARGET 2~~~~~");/*End Target 2*/
+  console.log(" ")
 });
