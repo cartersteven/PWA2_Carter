32ca76d9fc684ff21c47a019e8b0f9626e9dcb47
Steven Carter <stevencarter@fullsail.edu>
2015-09-11 23:37:54 -0400

    Marked up  the modal

--- Week 2/main.js
+++ Week 2/main.js
@@ -1,36 +1,41 @@
 $(document).ready(function(){
   /* Tabs Action */
-  $('#search').hide();
+    $('#search').hide();
 
-  $('#aboutli').click(function(e){
-    e.preventDefault();
+    $('#aboutli').click(function(e){
+      e.preventDefault();
 
-      $('#content').children().hide('fast');
-      $('#about').show('fast');
-  });
+        $('#content').children().hide('fast');
+        $('#about').show('fast');
+    });
 
-  $('#searchli').click(function(e){
-    e.preventDefault();
+    $('#searchli').click(function(e){
+      e.preventDefault();
 
-      $('#content').children().hide('fast');
-      $('#search').show('fast');
-  });
+        $('#content').children().hide('fast');
+        $('#search').show('fast');
+    });
 
     /* Tooltip */
-    $('.masterTooltip').hover(function(){
-      var title = $(this).attr('title');
-      $(this).data('tiptext', title).removeAttr("title");
-      $('<p class="tooltip"></p>')
-      .text(title)
-      .appendTo('body')
-      .fadeIn('slow');
-    }, function(){
-      $(this).attr('title', $(this).data('tipText'));
-      $('.tooltip').remove();
-    }).mousemove(function(e){
-      var mousex = e.pageX + 20;
-      var mousey = e.pageY + 10;
-      $('.tooltip')
-      .css({top:mousey, left:mousex})
-    });
+      $('.masterTooltip').hover(function(){
+        var title = $(this).attr('title');
+        $(this).data('tiptext', title).removeAttr("title");
+        $('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo('body')
+        .fadeIn('slow');
+      }, function(){
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+      }).mousemove(function(e){
+        var mousex = e.pageX + 20;
+        var mousey = e.pageY + 10;
+        $('.tooltip')
+        .css({top:mousey, left:mousex})
+      });
+
+
+    /* Modal */
+      
+
 });
