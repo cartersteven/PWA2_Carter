b442d896781abb1cd7d8a47b059bbdbc64c91509
Steven Carter <stevencarter@fullsail.edu>
2015-09-18 18:13:37 -0400

    Added SVGs

--- Week 3/lname_fname_activity3/main.js
+++ Week 3/lname_fname_activity3/main.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    	$.ajax({                             
-    		url : "http://api.wunderground.com/api/ce24dab0c3bf6646/forecast/geolookup/conditions/q/FL/Melbourne.json",
-    		dataType : "json", 
-              success : function(parsed_json) { 
-                  var location = parsed_json['location']['city']; 
+$(document).ready(function () {
+    	$.ajax({
+            url : "http://api.wunderground.com/api/ce24dab0c3bf6646/forecast/geolookup/conditions/q/FL/Melbourne.json",
+            dataType : "json",
+              success : function (parsed_json) { 
+                  var location = parsed_json['location']['city'];
                   var temp_f = parsed_json['current_observation']['temp_f'];
-                      $(".current").html("Current temperature in " + location + " is: " + temp_f+"ºF");
+                  var currentLogo = parsed_json['current_observation']['icon_url'];
+                      $("#current_image").attr({
+  alt: "Beijing Brush Seller",
+  src: 'currentLogo'
+                      });
               } 
-    	}); 
+    	});
 });
