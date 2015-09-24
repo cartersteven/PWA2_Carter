2c5796a9267a4dca7ac0e2fa0ccb457702c471c8
Steven Carter <stevencarter@fullsail.edu>
2015-09-24 19:59:00 -0400

    Added all base code for modal
    If you screw this one up, restart from here.

--- Week 4/Activity/main.js
+++ Week 4/Activity/main.js
@@ -8,4 +8,23 @@ $(document).ready(function(){
     //Date Picker Code
     $( "#datepicker" ).datepicker();
 
+    //Modal
+    $('.modalClick').on("click", function(e){
+        e.preventDefault();
+        $('#overlay')
+          .fadeIn()
+          .find('#modal')
+          .fadeIn();
+          console.log("open");
+    });
+
+    $('.close').on('click', function(e){
+        e.preventDefault();
+        $('#overlay')
+          .fadeOut()
+          .find('#modal')
+          .fadeOut();
+          console.log("close");
+    });
+
 });
