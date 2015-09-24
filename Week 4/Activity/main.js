28344c7d1edbf644cb81f7a76727e79b7453ca65
Steven Carter <stevencarter@fullsail.edu>
2015-09-24 18:36:52 -0400

    Added code for date picker

--- Week 4/Activity/main.js
+++ Week 4/Activity/main.js
@@ -4,5 +4,8 @@ $(document).ready(function(){
     //Drag and drop code
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
+    
+    //Date Picker Code
+    $( "#datepicker" ).datepicker();
 
 });
